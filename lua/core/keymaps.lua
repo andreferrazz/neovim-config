adac5c4fd659fdeb0165afd0bf78f57229d0183f
André Ferraz <andre.ferraz@groupsoftware.com.br>
2023-12-20 21:38:44 -0300

    Initial commit

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,18 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = false
+vim.opt.autoread = true
+
+-- use spaces for tab and whatnot
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
---
+++ lua/core/plugin_config/nvim-tree.lua
@@ -0,0 +1,9 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("nvim-tree").setup()
+
+vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
+vim.keymap.set('n', '<c-h>', '<C-w>h')
+vim.keymap.set('n', '<c-l>', '<C-w>l')
+
