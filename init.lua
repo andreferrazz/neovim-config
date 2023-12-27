0860da07242b78c6a1f380b5981564ecb55d5936
André Ferraz <andre.ferraz@groupsoftware.com.br>
2023-12-27 15:39:45 -0300

    Add Comment.nvim plugin

--- init.lua
+++ init.lua
@@ -1,6 +1,3 @@
--- require("core.plugins")
--- require("core.plugin_config")
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
